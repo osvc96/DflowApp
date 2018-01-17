725b1342c19ee070aac76761e54fe18b6332d176
Oscar Alfredo Vega Cespedes <oscar.vega@avantica.net>
2018-01-17 16:59:51 -0600

    added text input/output

--- app/src/main/java/com/example/oscar/dflowapp/MainActivity.java
+++ app/src/main/java/com/example/oscar/dflowapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.oscar.dflowapp;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.os.Bundle;
 
 import ai.api.AIListener;
@@ -18,6 +21,7 @@ import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,29 +29,29 @@ import android.widget.TextView;
 
 import com.google.gson.JsonElement;
 
+import java.util.Locale;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements AIListener{
+public class MainActivity extends AppCompatActivity implements AIListener, OnInitListener{
 
-    private Button listenButton;
-    private Button readButton;
     private EditText editText;
     private TextView resultTextView;
-    private AIService aiService;
-    private AIDataService aiDataService;
+
     private final String CLIENT_ACCESS_TOKEN = "0e9d80a4712e4de48ba4cdf8347d9b72";
     private static final int REQUEST = 200;
+
+    private AIService aiService;
+    private AIDataService aiDataService;
     private AIError aiError;
 
+    private TextToSpeech tts;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         editText = (EditText) findViewById(R.id.editText);
-        listenButton = (Button) findViewById(R.id.listenButton);
-        readButton = (Button) findViewById(R.id.readButton);
         resultTextView = (TextView) findViewById(R.id.resultTextView);
 
         validateRecordingPermission();
@@ -60,7 +64,10 @@ public class MainActivity extends AppCompatActivity implements AIListener{
 
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
+
+        tts = new TextToSpeech(this, this);
     }
+
 
     private void validateRecordingPermission() {
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
@@ -68,47 +75,39 @@ public class MainActivity extends AppCompatActivity implements AIListener{
         }
     }
 
-    public void voiceButtonOnClick(final View view){
-        aiService.startListening();
-    }
+    @SuppressLint("StaticFieldLeak")
+    private AsyncTask<AIRequest, Void, AIResponse> sendRequestToDialogflow = new AsyncTask<AIRequest, Void, AIResponse>() {
+        @Override
+        protected AIResponse doInBackground(AIRequest... requests) {
+            final AIRequest request = requests[0];
+            try {
+                final AIResponse response = aiDataService.request(requests[0]);
+                return response;
+            } catch (AIServiceException e) {
+                aiError = new AIError(e);
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(AIResponse aiResponse) {
+            if (aiResponse != null) {
+                onResult(aiResponse);
+            }
+            else{
+                onError(aiError);
+            }
+        }
+    };
 
     public void textButtonOnClick(final View view) {
-
         final AIRequest aiRequest = new AIRequest();
-
         String query = editText.getText().toString();
-
         aiRequest.setQuery(query);
+        sendRequestToDialogflow.execute(aiRequest);
+    }
 
-
-
-        new AsyncTask<AIRequest, Void, AIResponse>() {
-            @Override
-            protected AIResponse doInBackground(AIRequest... requests) {
-                final AIRequest request = requests[0];
-                try {
-                    final AIResponse response = aiDataService.request(aiRequest);
-                    return response;
-                } catch (AIServiceException e) {
-                    aiError = new AIError(e);
-                }
-                return null;
-            }
-            @Override
-            protected void onPostExecute(AIResponse aiResponse) {
-                if (aiResponse != null) {
-                    onResult(aiResponse);
-                }
-                else{
-                    onError(aiError);
-                }
-            }
-        }.execute(aiRequest);
-
-        //aiService.startListening();
-
-
-
+    public void voiceButtonOnClick(final View view){
+        aiService.startListening();
     }
 
     @Override
@@ -128,6 +127,7 @@ public class MainActivity extends AppCompatActivity implements AIListener{
                 "\nSpeech: " + res.getFulfillment().getSpeech();
         // Show results in TextView.
         resultTextView.setText(message);
+        speakOut(res.getFulfillment().getSpeech());
     }
 
     @Override
@@ -154,4 +154,24 @@ public class MainActivity extends AppCompatActivity implements AIListener{
     public void onListeningFinished() {
 
     }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+            // tts.setPitch(5); // set pitch level
+            // tts.setSpeechRate(2); // set speech speed rate
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language is not supported");
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
+    }
+
+    private void speakOut(String text) {
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
 }
